491c2eb5c96e0c9be1288bb58256eb178719fac6
FinnnBak <finn.bak@hva.nl>
2024-09-18 23:29:29 +0200

    Buttons naar foto's toegevoegd
    Op de 2de pagina kan je nu tussen alle foto's scrollen

--- squad pagina/squad page web/script.js
+++ squad pagina/squad page web/script.js
@@ -1,9 +1,43 @@
 // Array of image paths or URLs
 const images = [
-    'personal-photo.png',
-    'profileCard.png',
-    '2de-pagina-laptopview.jpeg.png',
-    '2de-pagina-mobileview.jpeg'
+    
+    'Foto-iedereen-squad/Squad1H-Anouar.JPG',
+    'Foto-iedereen-squad/Squad1H-Anouk.JPG',
+    'Foto-iedereen-squad/Squad1H-Aria.JPG',
+    'Foto-iedereen-squad/Squad1H-Arman.JPG',
+    'Foto-iedereen-squad/Squad1H-Daan.JPG',
+    'Foto-iedereen-squad/Squad1H-Finn (5).JPG',
+    'Foto-iedereen-squad/Squad1H-kai.JPG',
+    'Foto-iedereen-squad/Squad1H-Kate.JPG',
+    'Foto-iedereen-squad/Squad1H-Kerem.JPG',
+    'Foto-iedereen-squad/Squad1H-Kyan (1).JPG',
+'Foto-iedereen-squad/Squad1H-Luuk.JPG', 
+'Foto-iedereen-squad/Squad1H-Marije.JPG', 
+'Foto-iedereen-squad/Squad1H-Miel.JPG',
+'Foto-iedereen-squad/Squad1H-Mkiyas.JPG', 
+'Foto-iedereen-squad/Squad1H-Nawal.JPG',
+'Foto-iedereen-squad/Squad1H-Nayome.JPG', 
+'Foto-iedereen-squad/Squad1H-Rapha.JPG',
+'Foto-iedereen-squad/Squad1H-Renzo.JPG',
+'Foto-iedereen-squad/Squad1H-Reyhan (1).JPG',
+'Foto-iedereen-squad/Squad1H-Rick (1).JPG',
+'Foto-iedereen-squad/Squad1H-Riley.JPG',
+'Foto-iedereen-squad/Squad1H-Safoe.JPG',
+'Foto-iedereen-squad/Squad1H-Sascha.JPG',
+'Foto-iedereen-squad/Squad1H-Sebastiaan.JPG', 
+'Foto-iedereen-squad/Squad1H-Senne (1).JPG', 
+'Foto-iedereen-squad/Squad1H-Sidney.JPG',
+'Foto-iedereen-squad/Squad1H-Sjors.JPG', 
+'Foto-iedereen-squad/Squad1H-Stella.JPG', 
+'Foto-iedereen-squad/Squad1H-Suleyman.JPG', 
+'Foto-iedereen-squad/Squad1H-Tessa.JPG', 
+'Foto-iedereen-squad/Squad1H-Yamen (1).JPG', 
+
+
+
+
+
+
 ];
 
 let currentIndex = 0;
@@ -29,3 +63,5 @@ document.getElementById('prev').addEventListener('click', function(e) {
     currentIndex = (currentIndex - 1 + images.length) % images.length; // Loop to the last image if needed
     updateImage(currentIndex);
 });
+
+
